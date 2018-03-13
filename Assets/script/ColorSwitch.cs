ded1f92945a57f5392c0ab96b37d7d0f899a3804
outtaken <liubko81@gmail.com>
2018-03-14 00:01:45 +0300

    Normal part 2

---
+++ Assets/script/ColorSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSwitch : MonoBehaviour {
+	Material m_Material;
+
+	// Use this for initialization
+	void Start () {
+		m_Material = GetComponent<Renderer> ().material;
+		m_Material.color = Color.magenta;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
